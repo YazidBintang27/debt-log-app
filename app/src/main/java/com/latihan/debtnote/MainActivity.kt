6c78776d8e76c1ba2dce57cc24b8daa359356023
Yazid Bintang <yazidbintang2711@gmail.com>
2024-08-30 22:07:23 +0700

    App run correctly

--- app/src/main/java/com/latihan/debtnote/MainActivity.kt
+++ app/src/main/java/com/latihan/debtnote/MainActivity.kt
@@ -1,20 +1,103 @@
 package com.latihan.debtnote
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.latihan.debtnote.adapter.DebtCardAdapter
+import com.latihan.debtnote.database.entity.Debt
+import com.latihan.debtnote.databinding.ActivityMainBinding
+import com.latihan.debtnote.screens.AddDataActivity
+import com.latihan.debtnote.screens.UpdateDataActivity
+import com.latihan.debtnote.viewmodel.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+   private lateinit var binding: ActivityMainBinding
+   private val homeViewModel: HomeViewModel by viewModels()
+   private val debtCardAdapter: DebtCardAdapter = DebtCardAdapter()
+
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       enableEdgeToEdge()
-      setContentView(R.layout.activity_main)
+      binding = ActivityMainBinding.inflate(layoutInflater)
+      setContentView(binding.root)
       ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
          val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
          v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
          insets
       }
+      ViewCompat.setOnApplyWindowInsetsListener(binding.myToolbar) { v, insets ->
+         val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+         insets
+      }
+      setupActionBar()
+      setupAdapter()
+      getAllData()
+      addData()
+      deleteData()
+   }
+
+   private fun setupActionBar() {
+      setSupportActionBar(binding.myToolbar)
+   }
+
+   private fun setupAdapter() {
+      binding.rvDebtCard.apply {
+         adapter = debtCardAdapter
+         setHasFixedSize(true)
+         layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+      }
+      debtCardAdapter.setOnClickCallback(object: DebtCardAdapter.OnItemClickCallback {
+         override fun onItemClicked(index: Int) {
+            val intent = Intent(this@MainActivity, UpdateDataActivity::class.java)
+            intent.putExtra(UpdateDataActivity.EXTRA_DATA_INDEX, index)
+            startActivity(intent)
+         }
+      })
+   }
+
+   private fun getAllData() {
+      lifecycleScope.launch {
+         homeViewModel.allData.collectLatest {
+            debtCardAdapter.setData(it)
+         }
+      }
+   }
+
+   private fun addData() {
+      binding.fabAddData.setOnClickListener {
+         val intent = Intent(this, AddDataActivity::class.java)
+         startActivity(intent)
+      }
+   }
+
+   private fun deleteData() {
+      debtCardAdapter.setOnDeleteCallback(object: DebtCardAdapter.OnDeleteClickCallback {
+         override fun onDeleteClicked(debt: Debt) {
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setMessage("Apakah anda yakin menghapus data ini?")
+               .setPositiveButton("Ya") { dialog, id ->
+                  homeViewModel.deleteData(debt)
+               }
+               .setNegativeButton("Batal") { dialog, id ->
+                  dialog.dismiss()
+               }
+            builder.create().show()
+         }
+      })
    }
 }
